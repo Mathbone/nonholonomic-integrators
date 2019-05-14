64c429553fbe54786ac9a8cb5503f1219d109162
Brandon Wilson <pandamathium@gmail.com>
2019-05-14 17:25:15 -0600

    fixed integrator

--- funcs.py
+++ funcs.py
@@ -33,11 +33,13 @@ def funcCreator(lagrangian,variables,constants,constraintMatrix,control):
     
     pairs1 = []
     pairs2 = []
+    pairs3 = []
     for i in range(len(variables)):
         pairs1.append((v[i],(v1[i]+v0[i])/2))
         pairs1.append((vdot[i],(v1[i]-v0[i])/h))
         pairs2.append((v[i],(v2[i]+v2[i])/2))
         pairs2.append((vdot[i],(v2[i]-v1[i])/h))
+        pairs3.append((v[i],v1[i]))
     
     Ld1 = h*L.subs(pairs1)
     Ld2 = h*L.subs(pairs2)
@@ -46,30 +48,39 @@ def funcCreator(lagrangian,variables,constants,constraintMatrix,control):
     for i in range(len(variables)):
         forces = 0
         for j in range(len(A)):
-            forces+=lmbda[j]*A[j][i]
+            forces+=lmbda[j]*(A[j][i].subs(pairs3))
         if control[i]:
             forces+= u[i]   
         eq.append((sympy.diff(Ld1,v1[i])+sympy.diff(Ld2,v1[i])-forces))
     
     for row in omega:
-        eq.append(row)
+        eq.append(row.subs(pairs2))
         
-    print(eq)
+    uActual = []
+    for i in range(len(u)):
+        if control[i]:
+            uActual.append(u[i])
         
-    f = sympy.lambdify(list(v2)+[h]+list(v1)+list(v0)+list(c),eq,"numpy")
+    f = sympy.lambdify(list(v2)+list(lmbda)+[h]+list(v1)+list(v0)+list(uActual)+list(c),eq,"numpy")
     
     def func(x,y):
         return f(*flatten((x,y)))
     
     return func
 
-def integratorCreator(func, x0, xdot0, k, h, numIter):
-    x = numpy.zeros((numIter,len(x0)))
+
+    
+
+def integratorCreator(func, x0, xdot0, u, k, h, numCons, numIter):
+    x = numpy.zeros((numIter,len(x0)+numCons))
+    for i in range(numCons):
+        x0.append(0)
+        xdot0.append(0)
     x[0] = numpy.array(x0)
     x[1] = numpy.array(xdot0)*h+x[0]
     
     for i in range(2,len(x)):
-        x[i] = fsolve(func,2*x[i-1]-x[i-2],args = [h]+list(x[i-1])+list(x[i-2])+k)
+        x[i] = fsolve(func,2*x[i-1]-x[i-2],args = [h]+list(x[i-1][:len(x0)])+list(x[i-2][:len(x0)])+list(u[i-1])+k)
     return x
     
     
--- script.py
+++ script.py
@@ -18,13 +18,18 @@ L = ".5*M*((xdot+R*thetadot*cos(theta)*cos(phi)-R*phidot*sin(theta)*sin(phi))**2
 
 A = [["1","0","0","-R*cos(phi)","0"],["0","1","0","-R*sin(phi)","0"]]
 
-func = funcs.funcCreator(L,var,consts,A,[False,False,True,True,True])
+availableControls = [False,False,True,True,True]
 
-x0 = [0,0,0,0,numpy.pi/2]
-xdot0 = [0,5,0,5,0]
+func = funcs.funcCreator(L,var,consts,A,availableControls)
+
+numIter=100
+x0 = [0,0,0,0,0]
+xdot0 = [1,0,0,1,0]
 k = [1,2/5,3/5,1,9.8]
 h = .1
+u = numpy.zeros((numIter,sum(availableControls)))
 
-#x = funcs.integratorCreator(func,x0,xdot0,k,h,100)
+x = funcs.integratorCreator(func,x0,xdot0, u, k, h, 2, numIter)
 
-#plt.plot(x[:,0],x[:,1])
+plt.plot(x[:,0],x[:,1])
+
